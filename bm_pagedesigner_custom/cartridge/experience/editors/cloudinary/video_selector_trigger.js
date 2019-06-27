4d62d71ae3c93c787db22497628782d9c22658a6
Steffen Eckardt <seckardt@salesforce.com>
2019-06-27 09:05:57 +0200

    feat: refactor to utilize new breakout elements feature

--- bm_pagedesigner_custom/cartridge/experience/editors/cloudinary/video_selector_trigger.js
+++ bm_pagedesigner_custom/cartridge/experience/editors/cloudinary/video_selector_trigger.js
@@ -1,8 +1,13 @@
 'use strict';
 
+var HashMap = require('dw/util/HashMap');
+var PageMgr = require('dw/experience/PageMgr');
 var cloudinaryApi = require('~/cartridge/scripts/cloudinary/cloudinary_api');
 
 module.exports.init = function (editor) {
 	var jsonString = JSON.stringify(cloudinaryApi.getVideoJSON());
 	editor.configuration.put('fileData', jsonString);
+
+	var videoSelector = PageMgr.getCustomEditor('cloudinary.video_selector', new HashMap());
+	editor.dependencies.put('breakout', videoSelector);
 };
