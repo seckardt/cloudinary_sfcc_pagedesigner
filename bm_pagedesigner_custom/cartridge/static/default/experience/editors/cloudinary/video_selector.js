4886ce0bd2829a9bdab9b0a456912a75a6904dd1
Steffen Eckardt <seckardt@salesforce.com>
2019-09-09 14:46:03 +0200

    feat: dummy impl using client-side SHA-256 to open media library
    WARNING: don't use this code as is. It's a playground to ensure that the media library can be directly opened inside a custom editor breakout modal. To productise this code, please switch to creating the signature on the server-side.

--- bm_pagedesigner_custom/cartridge/static/default/experience/editors/cloudinary/video_selector.js
+++ bm_pagedesigner_custom/cartridge/static/default/experience/editors/cloudinary/video_selector.js
@@ -1,75 +1,40 @@
 (() => {
-	// Page Designer ready event
-	subscribe('sfcc:ready', async ({ value, config, isDisabled, isRequired, dataLocale, displayLocale }) => {
-		console.log('cloudinary.video_selector::sfcc:ready', dataLocale, displayLocale, isDisabled, isRequired, value, config);
+	const SECRET_CLOUD_NAME = 'xxx';
+	const SECRET_API_KEY = 'xxx';
+	const SECRET_USER_NAME = 'xxx@xxx';
+	const SECRET_TIMESTAMP = Date.now();
 
-		const template = obtainTemplate(config, value);
+	// Page Designer ready event
+	subscribe('sfcc:ready', async ({ value, config, isDisabled, isRequired, dataLocale, displayLocale, viewport }) => {
+		console.log('cloudinary.video_selector::sfcc:ready', dataLocale, displayLocale, isDisabled, isRequired, value, config, viewport);
+
+		const template = obtainTemplate(viewport);
 		const clone = document.importNode(template.content, true);
 		document.body.appendChild(clone);
 
-		// Apply `click` listeners to all selector items
-		const selectorContainerEl = document.querySelector('.video_selector__container');
-		const selectorItemEls = selectorContainerEl.querySelectorAll('.video_selector__item');
-		selectorItemEls.forEach(selectorItemEl => {
-			selectorItemEl.addEventListener('click', () => {
-				// Update `selected` class
-				selectorContainerEl.querySelectorAll('.selected').forEach(itemEl => itemEl.classList.remove('selected'));
-				selectorItemEl.classList.add('selected');
-
-				// Apply the selected value into the breakout sandbox container
-				emit({
-					type: 'sfcc:value',
-					payload: {
-						video_id: selectorItemEl.dataset.value
-					}
+		window.ml = cloudinary.openMediaLibrary({
+			cloud_name: SECRET_CLOUD_NAME,
+			api_key: SECRET_API_KEY,
+			username: SECRET_USER_NAME,
+			timestamp: SECRET_TIMESTAMP,
+			signature: sha256(`cloud_name=${SECRET_CLOUD_NAME}&timestamp=${SECRET_TIMESTAMP}&username=${SECRET_USER_NAME}`),
+			inline_container: 'div.sfcc-ml-root',
+			remove_header: true,
+			max_files: 8
+		}, {
+			insertHandler: (data) => {
+				data.assets.forEach(asset => {
+					console.log('Inserted asset:', JSON.stringify(asset, null, 2));
 				});
-			});
-		});
+			}
+		})
+
 	});
 
-	// Create an image URL with formatting options for thumbnails
-	function imageTransform(option) {
-		const { version, secure_url, format } = option;
-		const arr = secure_url.split('v' + version); // Remove version number
-		arr.splice(1, 0, 'c_lpad,h_50,w_50'); // Inject settings
-		return arr.join('').replace('.' + format, '.jpg');
-	}
-
-	function obtainItemMarkup(option, isSelected) {
-		const url = imageTransform(option);
-		return `<div class="video_selector__item${isSelected ? ' selected' : ''}" data-value="${option.public_id}">
-  <div class="video_selector__image">
-    <img src="${url}" />
-  </div>
-  <div class="video_selector__data">
-    <span class="video_selector__data__id">${option.public_id}</span><br />
-    <span class="video_selector__data__type">${option.resource_type}</span> - 
-    <span class="video_selector__data__format">${option.format}</span> - 
-    <span class="video_selector__data__size">${option.width} x ${option.height}</span>
-  </div>
-</div>`;
-	}
-
-	// Output HTML for video selector interface
-	function obtainTemplate(config, value) {
-		const selectedVideoId = typeof value === 'object' && value !== null && typeof value.video_id === 'string' ? value.video_id : null;
-		const fileData = JSON.parse(config.fileData);
-		const itemsMarkup = fileData.resources.reduce((acc, option) => {
-			acc += obtainItemMarkup(option, selectedVideoId === option.public_id);
-			return acc;
-		}, '');
-
+	function obtainTemplate(viewport) {
 		const template = document.createElement('template');
-		template.innerHTML = `
-<div class="video_selector__header">
-  <div class="video_selector__header__icon"></div>
-  <div class="video_selector__header__text">Powered by Cloudinary</div>
-</div>
-
-<div class="video_selector__container">
-${itemsMarkup}
-</div>`.trim();
-
+		const height = viewport.height - 16; // 16px = padding top + padding bottom
+		template.innerHTML = `<div class="sfcc-ml-root" style="height: ${height}px; max-height: ${height}px;"></div>`;
 		return template;
 	}
 })();
